c4f16181ea3c4ee3476128c11d67b2424bce9c19
Robert Lopez <robert.lopez@yale.edu>
2020-05-18 21:27:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,26 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    coupon.each do |attribute, value|
+      name = coupon[:item]
+      if find_item_by_name_in_collection(name, cart) && find_item_by_name_in_collection[:count] >= coupon[:num] 
+        index2 = 0 
+        while index2 < cart.length 
+        if cart[index2][:item] == "#{name} W/COUPON"
+          cart[index2][:count] += 1 
+        end 
+          index2 += 1 
+        end 
+        else cart << {:item => "#{name} W/COUPON", :price => coupon[:cost] / coupon[:num], :clearance => cart[index][:clearance],
+          :count => 1} 
+        end 
+        cart[index][:count] -= coupon[:num] 
+        
+    end 
 end
+end
+end 
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
